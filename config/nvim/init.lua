3a44dcb1eac754883c05f689510f2fb5a9ed31db
pichus <dimatyzuk@gmail.com>
2026-06-06 10:51:34 +0300

    configure format on save

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,3 +3,10 @@ require('config.lazy')
 require('lsp')
 require('keymaps')
 
+-- format on save
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function()
+    vim.lsp.buf.format({ async = false })
+  end,
+})
